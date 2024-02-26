3b171447a9042f6d8cafb914ec72bef009710fe5
nuno <nuno@nvideira.nvideira.42.fr>
2024-02-26 09:25:59 +0000

    nginx and mariadb mostly ready

---
+++ srcs/requirements/mariadb/tools/db-start.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+if [ ! -d "/var/lib/mysql/$DB_NAME"]; then
+
+echo "Starting mysql"
+exec service mysql start
+
+echo "Creating database $DB_NAME"
+mysql -u root -e "CREATE DATABASE $DB_NAME;"
+
+echo "Creating user $DB_USR"
+mysql -u root -e "CREATE USER '$DB_USR'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+
+echo "Applying privileges to $DB_USR"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USR'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+echo "Setting root password"
+mysqladmin -u root password "$DB_PASSWORD"
+mysqladmin -u root -p"$DB_PASSWORD" shutdown
+
+fi
+
+exec mysqld_safe --bind-address=0.0.0.0
